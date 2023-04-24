5c5d05662c3174e7031737fe2b51bd61011739f5
Des G Bauza <dgb.javadeveloper@gmail.com>
2023-04-24 11:13:08 -0400

    CreatingPOJOSforBothTables

---
+++ template_java_project/src/com/bloomtech/zodiakProject/User.java
@@ -0,0 +1,51 @@
+public class User{
+    private String userId;
+    private String userName;
+    private String birthDate;
+    private String pronouns;
+
+
+    public String getUserId(){
+        return userName();
+
+    }
+
+
+    public void setUserId(){
+        this.userId = userId;
+
+    }
+
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getPronouns() {
+        return pronouns;
+    }
+
+    public void setPronouns(String pronouns) {
+        this.pronouns = pronouns;
+    }
+
+
+
+
+}
+
+
+
+
---
+++ template_java_project/src/com/bloomtech/zodiakProject/UserSign.java
@@ -0,0 +1,12 @@
+public class User {
+
+    private
+
+
+
+
+
+
+}
+
+
