ba2052f104839c1ace61142372dac5c6d1102fa8
Des G Bauza <dgb.javadeveloper@gmail.com>
2023-04-24 11:40:38 -0400

    AddedUserDao_SkeletonClass

--- template_java_project/src/com/bloomtech/zodiakProject/UserDao.java
+++ template_java_project/src/com/bloomtech/zodiakProject/UserDao.java
@@ -1,4 +1,29 @@
 
+public Class UserDao(){
+    private final DynamoDbMapper dynamoDbMapper;
+
+
+    @Inject
+    public UserDao(DynamoDBMapper,dynamoDbMapper){
+        this.dynamoDbMapper=dynamoDbMapper;
+
+        }
+
+
+    public User getUser(String userId){
+
+    }
+
+    public void saveUser(User user){
+
+    }
+
+
+
+
+
+
+}
 
 
 
