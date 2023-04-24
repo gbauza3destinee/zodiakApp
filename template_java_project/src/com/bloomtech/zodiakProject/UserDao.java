0ba2482fe5bebf8dad024f55dfa8fc50ba706b78
Des G Bauza <dgb.javadeveloper@gmail.com>
2023-04-24 12:04:41 -0400

    AddedDynamoDBInstantiationStatement_intoDaoClassConstructor

--- template_java_project/src/com/bloomtech/zodiakProject/UserDao.java
+++ template_java_project/src/com/bloomtech/zodiakProject/UserDao.java
@@ -3,9 +3,12 @@ public Class UserDao(){
     private final DynamoDbMapper dynamoDbMapper;
 
 
+    // TODO : Why is this not formatting correctly?
     @Inject
     public UserDao(DynamoDBMapper,dynamoDbMapper){
-        this.dynamoDbMapper=dynamoDbMapper;
+        AmazonDynamoDB dynamoDbClient = DynamoDbClientProvider.getDynamoDBClient();
+        this.dynamoDbMapper = dynamoDbMapper;
+
 
         }
 
