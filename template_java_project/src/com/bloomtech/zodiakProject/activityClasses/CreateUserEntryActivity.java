334b5fd48385871005f939c202637670c2cffd6c
Des G Bauza <dgb.javadeveloper@gmail.com>
2023-04-26 12:28:20 -0400

    ImplementedRequestHandlerInterfaceAndLogic_ActivityClass

--- template_java_project/src/com/bloomtech/zodiakProject/activityClasses/CreateUserEntryActivity.java
+++ template_java_project/src/com/bloomtech/zodiakProject/activityClasses/CreateUserEntryActivity.java
@@ -5,7 +5,6 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.bloomtech.zodiakProject.ModelClasses.Requests.CreateUserRequest;
 import com.bloomtech.zodiakProject.ModelClasses.Results.CreateUserResult;
 import com.bloomtech.zodiakProject.ModelClasses.User;
-import com.bloomtech.zodiakProject.ModelClasses.UserModel;
 import com.bloomtech.zodiakProject.UserDao;
 
 import java.text.DateFormat;
