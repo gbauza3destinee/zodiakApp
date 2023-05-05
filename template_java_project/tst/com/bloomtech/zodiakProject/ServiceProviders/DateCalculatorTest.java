12b91877214aaba309c5c50a6ab5662034163f59
Des G Bauza <dgb.javadeveloper@gmail.com>
2023-05-05 14:39:19 -0400

    TestDirectorySet_DateCalculatorTestClass_CreatedWithEdgeCaseAndHappyCase_EmptySkeleton:

--- template_java_project/tst/com/bloomtech/zodiakProject/ServiceProviders/DateCalculatorTest.java
+++ template_java_project/tst/com/bloomtech/zodiakProject/ServiceProviders/DateCalculatorTest.java
@@ -1,4 +1,62 @@
 package com.bloomtech.zodiakProject.ServiceProviders;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+
+// import static org.junit.jupiter.api.Assertions;
 
 public class DateCalculatorTest {
+
+
+    @Mock
+    DateCalculator dateCalculator;
+
+
+
+    @BeforeEach{
+        dateCalculator = new DateCalculator();
+
+    }
+
+    // Test : Happy Case 1
+    @Test
+    public void findUserZodiacAndElementalSign_UserProvidesValidBirthDate(LocalDate userBirthDate){
+
+
+
+
+    }
+
+
+    // Test : Edge Case 1
+    @Test
+    public void findUserZodiacAndElementalSign_UserProvidesInvalidBirthDate(LocalDate userBirthDate){
+
+
+
+
+    }
+
+    // Test : Edge Case 2
+
+
+    @Test
+    public void findUserZodiacAndElementalSign_UserProvidesInvalidCharacters(LocalDate userBirthDate){
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
 }
